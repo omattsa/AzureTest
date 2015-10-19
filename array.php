dcaaa72bc24538a907cd002664834115267ddc2c
omattsa <matt.sandham@hotmail.co.uk>
2015-10-19 16:08:30 +0100

    array activity

---
+++ array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+<p>
+    <?php
+   $provisionedActivities = array("Specs", "Drugs","Rock and Roll");
+
+    foreach($provisionedActivities as $x){
+        print "<p>$x</p>";
+    }
+    ?>
+</p>
+</body>
+</html>
