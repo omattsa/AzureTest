d30521c258a28a070536dc60ad573cc0372fb035
omattsa <matt.sandham@hotmail.co.uk>
2015-10-26 16:05:53 +0000

    Planets and URL linking

---
+++ helloPrinter.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+<p>
+    <?php
+
+    ?>
+</p>
+</body>
+</html>
---
+++ helloSelector.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello selector</title>
+</head>
+<body>
+
+<p>Where would you like to say hello?</p>
+
+<p><a href="helloPrinter.php?planet=Earth">Earth</a></p>
+
+<p><a href="helloPrinter.php?planet=Mars">Mars</a></p>
+
+<p><a href="helloPrinter.php?planet=Uranus">Uranus</a></p>
+
+
+
+</body>
+</html>
